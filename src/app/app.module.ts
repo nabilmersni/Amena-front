c21cdc733dcaa9376336ae3a0351d922f7b8335d
nabilmersni <nabilmersni123@gmail.com>
2020-07-13 15:00:14 +0100

    Ready to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/public/landing-page/landing-page.component';
+import { TestComponent } from './components/test/test.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    TestComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
